22f4f044d819db72ba697a4b17d7b85501362e61
TarasovAntonSergeevich <tarasovanton0202@gmail.com>
2023-11-07 00:22:52 +0300

    feat: added tests for product page

--- pages/locators.py
+++ pages/locators.py
@@ -11,3 +11,11 @@ class LoginPageLocators():
     REG_EMAIL_LINK = (By.CSS_SELECTOR, '[name="registration-email"]')
     REG_PASSWORD_LINK = (By.CSS_SELECTOR, '[name="registration-password1"]')
     REG_REPEAT_PASSWORD_LINK = (By.CSS_SELECTOR, '[name="registration-password2"]')
+
+
+class ProductPageLocators():
+    ADD_BUTTON_LINK = (By.CSS_SELECTOR, '[value="Добавить в корзину"]')
+    BOOK_NAME = (By.CLASS_NAME, "alertinner")
+    EXPECTED_NAME = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > h1")
+    BOOK_PRICE = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > p.price_color")
+    BASKET_PRICE = (By.CSS_SELECTOR, "#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong")
---
+++ test_product_page.py
@@ -0,0 +1,13 @@
+from .pages.product_page import ProductPage
+
+def test_guest_can_add_see_button(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_add_button()
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_in_basket()
